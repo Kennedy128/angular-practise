db573216be1edbc0f2eac80841df46560cc1a083
Santa claus <santa@northpole.com>
2020-04-11 18:14:18 +0300

    removal of errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,13 @@ import { Text } from './text';
 })
 export class AppComponent {
   texts:Text[] = [
-    {id:1, name:'Watch finding Nemo'},
-    {id:2,name:'Buy Cookies'},
-    {id:3,name:'Get new Phone Case'},
-    {id:4,name:'Get Dog Food'},
-    {id:5,name:'Solve math homework'},
-    {id:6,name:'Plot my world domination plan'},
+    new Text(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
+    new Text(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
+    new Text(3,'Get new Phones Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
+    new Text(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
+    new Text(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
+    new Text(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
+    
   ];
   
 }
